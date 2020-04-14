e73bf5aa7954dc938b5b2b3b9a32cd438cdee0fc
sebastianibam002 <sebastianibam@gmail.com>
2020-04-14 15:32:46 -0500

    Arboles Binarios Problem Se deja como tarea el hacer el remove, por nuestra parte

--- clases/ADT/ArbolesBusquedabinaria/binarytree.hpp
+++ clases/ADT/ArbolesBusquedabinaria/binarytree.hpp
@@ -11,6 +11,9 @@ struct BSTNode
   BSTNode<T> *left;
   //hijo derecho
   BSTNode<T> *right;
+  //nodo padre
+  BSTNode<T> *parent;
+  
 };
 
 template <typename T>
@@ -20,44 +23,163 @@ class BST
 private:
   //tenemos la raiz
   BSTNode<T> *root;
-  void display_node(BSTNode<T>* p_node);
+  void display_node(BSTNode<T>* p_node, int count);
+  void insert_node(BSTNode<T> *&node, T k, BSTNode<T>*p);
+  //no accedemos nunca desde afuera
+  //node desde donde comenzamos a destruir recursivamente
+  void destroy_recursive(BSTNode<T> *node);
+  BSTNode<T>* find_node(BSTNode<T> *node, T k);
+  //Minimo y maximo
+  BSTNode<T>* minimum(BSTNode<T> *t);
+  BSTNode<T>* maximum(BSTNode<T> *t);
+
 public:
   BST(){root = nullptr;}
-
+  ~BST(){destroy_recursive(root);} //destructor 
   //crea un nodo, y derecho e izquiedo apunta a nullptr
-  void insert(T p_key);
+  
   void display();
   bool empty();
-  
+  /*--------------*/
+  void test(T val);
+  void insert(T k){insert_node(root,k, nullptr);}
+  BSTNode<T>* find(T k){return find_node(root, k);}
+  //Predecesor y sucesor
+  BSTNode<T> *predecessor(BSTNode<T> *pNode); //le doy un nodo y me deberia retorna el predecesor de este
+  BSTNode<T> *sucessor(BSTNode<T> *pNode);
 };
 
+
+
+
+
 template<typename T>
-void BST<T>::insert(T p_key)
+BSTNode<T> *BST<T>::predecessor(BSTNode<T> *pNode)
 {
-  //creamos un nodo al inicio
-  BSTNode<T> *new_node = new BSTNode<T>;
-  new_node->key = p_key;
-  new_node->left = nullptr;
-  new_node->right = nullptr;
-  
-  if(empty())
+  //continua mientras siga siendo hijo izquierdo de su padre
+  if(pNode->left != nullptr)
     {
-      root = new_node;
+      return maximum(pNode->left);
     }
   else
     {
-      //si es mayor, se deja por la derecha
-      if(root->key < new_node->key)
+      while(((pNode->parent)->left == pNode) && (pNode->parent) != nullptr)
 	{
-	  root->right = new_node;
+	  pNode = pNode->parent;
 	}
-      else
-	{
-	  root->left = new_node;
-	}
-      
+
+      return pNode->parent;
     }
 }
+
+template<typename T>
+BSTNode<T> *BST<T>::sucessor(BSTNode<T> *pNode)
+{
+  if(pNode->right != nullptr)
+    {
+      return minimum(pNode->right);
+    }
+  else
+    {
+      while(((pNode->parent)->right == pNode) && (pNode->parent) != nullptr)
+	{
+	  pNode = pNode->parent;
+	}
+
+      return pNode->parent;
+    }
+
+}
+
+template<typename T>
+BSTNode<T> *BST<T>::minimum(BSTNode<T> *t)
+{
+
+  /*
+  //mientras que el de la izquierda sea diferente de nullptr
+  while(t->left != nullptr)
+    //t ahora se va cambiando al nodo que tiene a la izquierda asi va moviendose
+    t = t->left;
+
+  return t;
+  */
+  
+
+  if(t->left == nullptr)
+    {
+      return t;
+    }
+  else
+    {
+      minimum(t->left);
+    }
+}
+
+template<typename T>
+BSTNode<T> *BST<T>::maximum(BSTNode<T> *t)
+{
+  //mismo principio de minimo pero ahora hacia la derecha con maximun
+  /*
+  while(t->right != nullptr)
+    {
+      t = t->right;
+    }
+  return t;
+  */
+
+  if(t->right == nullptr)
+    {
+      return t;
+    }
+  else
+    {
+      maximum(t->right);
+    }
+}
+
+
+
+
+template<typename T>
+void BST<T>::insert_node(BSTNode<T> *&node, T k, BSTNode<T> *p)
+{
+
+  if(node == nullptr)
+    {
+      node = new BSTNode<T>;
+      node->key = k;
+      node->left = nullptr;
+      node->right = nullptr;
+      node->parent = p;
+    }
+  else
+    {
+      if(k != node->key)
+	{
+	  if(k < node->key)
+	    {
+	      insert_node(node->left, k, node);
+	    }
+	  else
+	    {
+	      insert_node(node->right, k, node);
+	    }
+	}
+    }
+
+}
+
+template<typename T>
+void BST<T>::test(T val)
+{
+
+  //std::cout<<"Lowest element: "<<minimum(root)->key<<std::endl;
+  //std::cout<<"Highest element: "<<maximum(root)->key<<std::endl;
+  std::cout<<"El predecessor de: "<<val<<" es "<<predecessor(find(val))->key<<std::endl;
+  std::cout<<"El sucessor de: "<<val<<" es "<<sucessor(find(val))->key<<std::endl;
+}
+
+
 
 //empty
 template<typename T>
@@ -68,30 +190,50 @@ bool BST<T>::empty()
 
 //muestra el arbol como nos esta quedando
 template<typename T>
-void BST<T>::display_node(BSTNode<T> *p_node)
+void BST<T>::display_node(BSTNode<T> *p_node, int count)
 {
   //recursion
-  
+  count++;
   if(p_node != nullptr)
     {
-      if(p_node->left != nullptr)
-	{
-	  display_node(p_node->left);
-	}
-      std::cout<<p_node->key<<" ";
- 
-      if(p_node->right != nullptr)
-	{
-	  display_node(p_node->right);
-	}
+
+      display_node(p_node->left, count);	
+      std::cout<< "("<<count -1<< ")"<<p_node->key<<" ";
+      display_node(p_node->right, count);
+	
     }
 }
 
 template<typename T>
+void BST<T>::destroy_recursive(BSTNode<T> *node)
+{
+  if(node != nullptr)
+    {
+      destroy_recursive(node->left);
+      destroy_recursive(node->right);
+      delete node;
+    }
+}
+
+template<typename T>
+BSTNode<T> * BST<T>::find_node(BSTNode<T> *node, T k)
+{
+  if(node == nullptr) return nullptr;
+  if(node->key == k) return node;
+  if(node->key < k)
+    return find_node(node->right, k);
+  else
+    return find_node(node->left, k);
+}
+
+
+
+template<typename T>
 void BST<T>::display()
 {
-  //wraper para no accer al root desde afuera
-  display_node(root);
+  //wraper para no acceder al root desde afuera
+  int contador = 0;
+  display_node(root, contador);
   std::cout<<std::endl;
 }
 #endif
--- clases/ADT/ArbolesBusquedabinaria/main.cpp
+++ clases/ADT/ArbolesBusquedabinaria/main.cpp
@@ -1,12 +1,42 @@
 #include "binarytree.hpp"
+#include <string>
 using namespace std;
 
 int main()
 {
-  BST<int> myTree;
+  /*
+  
+  BST<string> myTree;
 
-  myTree.insert(3);
-  myTree.insert(6);
+  myTree.insert("Grumpy");
+  myTree.insert("Bashful");
+  myTree.insert("Doc");
+  myTree.insert("Dopey");
+  myTree.insert("Happy");
+  myTree.insert("Sneezy");
+  myTree.insert("Sleepy");
   myTree.display();
+  string name = "Dopey";
+  myTree.test("Sneezy");
+  
+  */
+  
+  BST<int> myTree;
+  myTree.insert(30);
+  myTree.insert(16);
+  myTree.insert(40);
+  myTree.insert(1);
+  myTree.insert(20);
+  myTree.insert(36);
+  myTree.insert(-1);
+  myTree.insert(2);
+  myTree.insert(17);
+  myTree.insert(32);
+  myTree.insert(4);
+  myTree.insert(15);
+  myTree.display();
+  myTree.test(15);
+  myTree.test(32);
+  
   return 0;
 }
